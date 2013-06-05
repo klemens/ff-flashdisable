f8f768f9dc666a39e9036cafde0e455533df592e
Klemens Schölhorn <klemens@schoelhorn.eu>
2013-06-05 20:08:38 +0200

    Import old version 0.7

--- packages/toolbar-button-modded-toolbarbuttonjs-from-erik-vold/lib/index.js
+++
@@ -1,9 +0,0 @@
-/*
-   Slightly modded version of Erik Vold's Toolbar Button (toolbarbutton.js)
-
-   modified setIcon method
-   added setTooltip method
-   added tooltip property
-   onClick event listener
-
-*/
